9c489fe9637a642b0f5af264cfa150074e44d1f9
Mark Daniel <markdaniel1982@gmail.com>
2023-06-01 09:14:52 +0000

    create Admin models

--- cvapp/admin.py
+++ cvapp/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Page, Comment
 
-# Register your models here.
+
+@admin.register(Page)
+class PostAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'author', 'added_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'added_on')
+    summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'added_on', 'approved')
+    list_filter = ('approved', 'added_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
