72aa4c22848a41f3235840c37ab9a9da8878a5c0
Mark Daniel <markdaniel1982@gmail.com>
2023-06-01 12:55:27 +0000

    rebuild models.py

--- cvapp/admin.py
+++ cvapp/admin.py
@@ -1,22 +1,22 @@
 from django.contrib import admin
-from .models import Page, Comment
+from .models import Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
-@admin.register(Page)
-class PageAdmin(SummernoteModelAdmin):
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'author', 'added_on')
+    list_display = ('title', 'slug', 'status', 'created_on')
     search_fields = ['title', 'content']
-    list_filter = ('status', 'added_on')
+    list_filter = ('status', 'created_on')
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'body', 'added_on', 'approved')
-    list_filter = ('approved', 'added_on')
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
     search_fields = ('name', 'email', 'body')
     actions = ['approve_comments']
 
--- cvapp/models.py
+++ cvapp/models.py
@@ -1,21 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-STATUS = {(0, "Draft"), (1, "Published")}
+STATUS = ((0, "Draft"), (1, "Published"))
 
-class Page(models.Model):
+
+class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts", default="Mark Daniel")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts"
+    )
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    added_on = models.DateTimeField(auto_now=True)
-    status = models.IntegerField(choices=STATUS, default=1)
-    likes = models.ManyToManyField(User, related_name="likes", blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(
+        User, related_name='blogpost_like', blank=True)
 
     class Meta:
-        ordering = ['-added_on']
+        ordering = ["-created_on"]
 
-    def __str_(self):
+    def __str__(self):
         return self.title
 
     def number_of_likes(self):
@@ -23,15 +28,16 @@ class Page(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Page, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
-    added_on = models.DateTimeField(auto_now_add=True)
+    created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
 
     class Meta:
-        ordering = ["added_on"]
+        ordering = ["created_on"]
 
     def __str__(self):
         return f"Comment {self.body} by {self.name}"
