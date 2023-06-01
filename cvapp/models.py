253f2423e6f410a1a28f80ce54e6c1fe62025377
Mark Daniel <markdaniel1982@gmail.com>
2023-06-01 09:06:45 +0000

    add page and comment models

--- cvapp/models.py
+++ cvapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = {(0, "Draft"), (1, "Published")}
+
+class Page(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts", default="Mark Daniel")
+    content = models.TextField()
+    added_on = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=1)
+    likes = models.ManyToManyField(User, related_name="likes", blank=True)
+
+    class Meta:
+        ordering = ['-added_on']
+
+    def __str_(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Page, on_delete=models.CASCADE, related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
